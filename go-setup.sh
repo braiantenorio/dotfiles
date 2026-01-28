945a396660f05b7a8c309974c360d77e3eac89d7
Braian Tenorio <tenoriovidalbraian@gmail.com>
2026-01-28 19:37:29 -0300

    go setup script fix

--- go-setup.sh
+++ go-setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Dirty dirty dirty
-VERSION="1.21.1"
-
 source .bashrc
-wget https://storage.googleapis.com/golang/go$VERSION.linux-amd64.tar.gz
-tar xvfz go$VERSION.linux-amd64.tar.gz
+source .zshrc
+
+VERSION=$(curl -s https://go.dev/VERSION?m=text)
+
+wget https://go.dev/dl/go$VERSION.linux-amd64.tar.gz
+
+#tar xvfz go$VERSION.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go
 sudo mv go /usr/local/
 
