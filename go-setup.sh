522a2017ce8dfad069baee24b4d1fb71d1236c77
Braian Tenorio <tenoriovidalbraian@gmail.com>
2026-02-04 16:52:54 -0300

    fix go-setup version retrieve

--- go-setup.sh
+++ go-setup.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-source .bashrc
-source .zshrc
 
-VERSION=$(curl -s https://go.dev/VERSION?m=text)
+VERSION=$(curl -s https://go.dev/VERSION?m=text | head -n 1)
 
-wget https://go.dev/dl/go$VERSION.linux-amd64.tar.gz
+wget https://go.dev/dl/$VERSION.linux-amd64.tar.gz
 
-#tar xvfz go$VERSION.linux-amd64.tar.gz
+tar xvfz $VERSION.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go
 sudo mv go /usr/local/
 
 go install golang.org/x/tools/cmd/goimports@latest
 go install github.com/nsf/gocode@latest
 
-rm go$VERSION.linux-amd64.tar.gz
+rm $VERSION.linux-amd64.tar.gz
